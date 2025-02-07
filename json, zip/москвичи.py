22ae438ab8c555e12d4561230ed7794a08c39ff4
mac <zerg@mail.ru>
2025-02-08 00:43:56 +0700

    москвичи

--- json, zip/москвичи.py
+++ json, zip/москвичи.py
@@ -1,17 +1,18 @@
+from re import search
 from zipfile import ZipFile
 import json
 
 count = 0
-with ZipFile('moscow 2.zip') as myzip:
+with ZipFile('moscow.zip') as myzip:
     for z in myzip.filelist:
         name = z.filename
         # print('  ' * (name.count('/')) + z.orig_filename.split('/')[-1])
         # print((z.orig_filename.split('/')[-1])[0])
         # print(name)
         if name[-5::] == '.json' and (z.orig_filename.split('/')[-1])[0] != '.':
-            with open(str(name)) as search_moscow:
-                data = json.load(search_moscow)
-                # print(data['city'])
-                if data['city'] == 'Москва':
-                    count += 1
+            search_moscow = open(name)
+            data = json.load(search_moscow)
+            # print(data['city'])
+            if data['city'] == 'Москва':
+                count += 1
 print(count)
